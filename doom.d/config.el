0c7a8a968a7c30ad67df43358b63206bf2789e8c
vinpopulaire <vinpopulaire@gmail.com>
2024-12-31 16:04:21 +0200

    [doom] Stop autoformatting on save and change font

--- doom.d/config.el
+++ doom.d/config.el
@@ -34,6 +34,8 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 14))
+
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
