94d08e5a6cabbeda3276bf9cb179b522555b31b5
Giorgos Mitsis <giorgos.mitsis@workable.com>
2025-03-06 10:13:48 +0200

    [nvim] fix typo for treesitter plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "nvim-tressitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
       require'nvim-treesitter.configs'.setup {
