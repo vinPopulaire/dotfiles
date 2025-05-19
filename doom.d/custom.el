d319de9d017b4fc2c4492db541a31ec9925e4a1c
Giorgos Mitsis <giorgos.mitsis@workable.com>
2025-05-19 14:42:07 +0300

    [doom] Add catppuchin themes

--- doom.d/custom.el
+++ doom.d/custom.el
@@ -3,15 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("e8ceeba381ba723b59a9abc4961f41583112fc7dc0e886d9fc36fa1dc37b4079" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" "046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" "691d671429fa6c6d73098fc6ff05d4a14a323ea0a18787daeb93fde0e48ab18b" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" default))
- '(org-agenda-files '("~/org/todo.org")))
+ '(package-selected-packages '(transient-dwim)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(doom-modeline-buffer-modified ((t (:foreground "orange")))))
+ )
 
 (custom-set-faces!
   '(doom-modeline-buffer-modified :foreground "orange"))
--- doom.d/packages.el
+++ doom.d/packages.el
@@ -61,3 +61,8 @@
 
 (package! copilot
   :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
+
+(package! catppuccin-theme)
+
+;; (package! copilot-chat
+;;   :recipe (:host github :repo "copilot-emacs/copilot-chat" :files ("*.el" "dist")))
